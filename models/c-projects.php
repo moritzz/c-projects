ceff8669815662655361f0ebfbb5cba64ca595f7
moritzz <mz@ia.net>
2018-08-16 11:14:32 +0200

    Latest WIP.

--- models/c-projects.php
+++ models/c-projects.php
@@ -10,14 +10,25 @@ if (!class_exists('ContaineristPage')) {
   $containerist_model_path = $plugin_dir_path . '/containerist/models/containerist.php';
   if (file_exists($containerist_model_path)) {
     require_once($containerist_model_path);
+  } else {
+    throw new Exception('Containerist plugin missing for containerist plugin Projects.');
   }
 }
 
 class CProjectsPage extends ContaineristPage {
   
-  public function categories() {
+  static function categories() {
     $blueprint = data::read(kirby()->get('blueprint', 'c-projects'));
-    $categories = $blueprint['fields']['projects']['fields']['category']['options'];
+    $categories_raw = $blueprint['fields']['projects']['fields']['category']['options'];
+    $categories_en = array_keys($categories_raw);
+    array_walk($categories_en, function (&$item, $key) {
+      $item = ucfirst($item);
+    });
+    $categories_data = new Data([
+      'en'=> $categories_en,
+      'de' => array_values($categories_raw),
+    ]);
+    $categories = new Structure($categories_data);
     var_dump($categories);
   }
   
