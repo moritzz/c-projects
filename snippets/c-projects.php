314562c6bdcadda4fc3c66f33b2794686d19c8ce
Moritz Zimmer <mz@ia.net>
2018-08-17 13:42:50 +0200

    Added project category filter, sub-snippet and empty result set statements.

--- snippets/c-projects.php
+++ snippets/c-projects.php
@@ -1,15 +1,29 @@
-    <section class="news-section">
+    <section class="projects-section">
       
       <div class="text wrap">
-        <?php if ($site->language() == 'de'): ?>
+        <?php if ($site->language()->code() == 'de'): ?>
           <h2>Projekte</h2>
         <?php else: ?>
           <h2>Latest Projects</h2>
         <?php endif; ?>
-        <?php foreach($field->toStructure()->sortBy('date_from', 'asc', 'date_to', 'asc')->flip() as $projects): ?>
-          <h3><?= datespan($projects->date_from(), $projects->date_to()); ?>: <?= $projects->title()->html(); ?></h3>
-          <?= $projects->summary()->kirbytext(); ?>
-        <?php endforeach; ?>
+        <?php
+          $selector = $kirby->request()->get('category');
+          $projects = (is_null($selector)) ? $page->projects()->toStructure() : $page->projects()->toStructure()->filterBy('category', urldecode($selector));
+          
+          if ($projects->count() > 0):
+            foreach($projects->sortBy('date_from', 'asc', 'date_to', 'asc')->flip() as $project) {
+              snippet('c-project', ['project' => $project]);
+            }
+          ?>
+        <?php else: ?>
+          <p class="result empty">
+          <?php if ($site->language()->code() == 'de'): ?>
+            Es wurden keine Projekte mit dieser Kategorie gefunden.
+          <?php else: ?>
+            We did not found any projects within this category.
+          <?php endif; ?>
+          </p>
+        <?php endif; ?>
       </div>
       
     </section>
