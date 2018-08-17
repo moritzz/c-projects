eb8a173817694d79190a2102f0b2c46bd7122061
Moritz Zimmer <mz@ia.net>
2018-08-17 13:44:12 +0200

    Moved project category filter and project output to snippets.

--- templates/c-projects.php
+++ templates/c-projects.php
@@ -1,5 +1,7 @@
 <?php snippet('header') ?>
-
+  
+  <?= css('assets/plugins/c-projects/css/index.css') ?>
+  
   <main class="main" role="main">
     
     <header class="wrap">
@@ -10,32 +12,9 @@
       <hr />
     </header>
     
-    <pre><?php $page->categories(); ?></pre>
+    <?php snippet('c-project-categories'); ?>
+    <?php snippet('c-projects'); ?>
     
-    <section class="news-section">
-      
-      <div class="text wrap">
-        <?php if ($site->language() == 'de'): ?>
-          <h2>Projekte</h2>
-        <?php else: ?>
-          <h2>Latest Projects</h2>
-        <?php endif; ?>
-        <?php $field = $page->projects()->toStructure()->sortBy('date_from', 'asc', 'date_to', 'asc')->flip(); ?>
-        <?php $groups = $page->projects()->toStructure()->groupBy('category'); ?>
-        <!-- <ul>
-        <?php foreach ($groups as $group => $items): ?>
-          <li><?= $group; ?></li>
-        <?php endforeach; ?>
-        </ul> -->
-        <?php foreach($field as $projects): ?>
-          <h3><?= datespan($projects->date_from(), $projects->date_to()); ?>: <?= $projects->title()->html(); ?></h3>
-          <?= $projects->summary()->kirbytext(); ?>
-        <?php endforeach; ?>
-      </div>
-      
-    </section>
-
-        
     <div class="text wrap">
       <?= $page->text()->kirbytext() ?>
     </div>
