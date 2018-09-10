5c790f5fd81b345ad414beb78b90a10f009d1009
Moritz Zimmer <mz@ia.net>
2018-09-10 15:07:02 +0200

    Fixed potential null key issue.

--- snippets/c-project-categories.php
+++ snippets/c-project-categories.php
@@ -8,7 +8,7 @@
           if ($page->categories()->isNotEmpty()) {
             foreach($page->categories()->toStructure() as $key => $category) {
               
-              if ($key == '%all%' || $all_projects->filterBy('category', urldecode($key))->count() > 0) {
+              if (!(is_null($key)) || $key == '%all%' || $all_projects->filterBy('category', urldecode($key))->count() > 0) {
                 echo $page->t($key)->link($page->url() . '?category=' . urlencode($key), ['class' => ('selector' . ((urldecode($key) == $selector) ? ' is-active' : ''))]) . ' ';
               }
               
