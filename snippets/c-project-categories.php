ba559affdd9c2f36e3a1a385281d758e6c613408
moritzz <mz@ia.net>
2018-09-27 06:14:29 +0200

    Fixed filtering of category filter.

--- snippets/c-project-categories.php
+++ snippets/c-project-categories.php
@@ -3,13 +3,13 @@
       <nav accesskey="p" class="text wrap e-categories">
         <?php 
           
-          $selector = $kirby->request()->get('category');
+          $selector = trim(urldecode($kirby->request()->get('category')));
           
           if ($page->categories()->isNotEmpty()) {
             foreach($page->categories()->toStructure() as $key => $category) {
               
-              if (!(is_null($key)) || $key == '%all%' || $all_projects->filterBy('category', urldecode($key))->count() > 0) {
-                echo $page->t($key)->link($page->url() . '?category=' . urlencode($key), ['class' => ('selector' . ((urldecode($key) == $selector) ? ' is-active' : ''))]) . ' ';
+              if (!(is_null($key) || $key == '%all%') && $all_projects->filterBy('category', $key)->count() > 0) {
+                echo $page->t($key)->link($page->url() . '?category=' . urlencode($key), ['class' => ('selector' . (($key == $selector) ? ' is-active' : ''))]) . ' ';
               }
               
             }            
