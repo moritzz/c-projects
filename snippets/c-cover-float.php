34840d9a85b5edaa87b13944ab5ae508ef2a514b
moritzz <mz@ia.net>
2018-10-29 19:48:31 +0100

    Added cover float html and styles.

--- snippets/c-cover-float.php
+++ snippets/c-cover-float.php
@@ -2,6 +2,7 @@
   <?php
     $file = $page->cover_image()->toFile();
     $image = $file->resize(720);
+    $image_height = $image->height();
     ?>
   <?php if ($page->cover_image()->isNotEmpty()): ?>
     <div class="c-cover is-float <?= ($has_intro) ? 'has-intro' : ''; ?> " style="background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('<?= $image->url() ?>'); background-position-x: center;  background-position-y: center; background-repeat: no-repeat; background-size: cover;">
